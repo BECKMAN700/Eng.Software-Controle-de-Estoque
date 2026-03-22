4920a13b1b8a04c9cdc1ac01bd37207625fa103e
murillo <97757137+murillofnandes@users.noreply.github.com>
2026-03-22 20:33:06 -0300

    feat: adiciona rota de listagem no index

--- public/index.php
+++ public/index.php
@@ -1,12 +1,21 @@
 <?php
+// Arquivo: public/index.php
 
 require_once __DIR__ . '/../app/Controllers/ProdutoController.php';
+require_once __DIR__ . '/../app/Models/ProdutoModel.php';
 
-$controller = new ProdutoController();
-$acao = $_GET['acao'] ?? '';
+$model = new ProdutoModel();
+$controller = new ProdutoController($model);
+
+$acao = $_GET['acao'] ?? 'listar';
 
 switch ($acao) {
+
+    case 'listar':
+        $controller->listar();
+        break;
+
     default:
         echo "Ação inválida.";
         break;
-}
+} 
