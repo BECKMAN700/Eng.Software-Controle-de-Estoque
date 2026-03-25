b4b4655382c3c83c4d81b240a87be2ef670dbdbe
João Pedro Rodrigues Bequiman <153242158+BECKMAN700@users.noreply.github.com>
2026-03-24 21:05:24 -0300

    Revert "Revert 4 feature/excluir produto"

---
+++ app/Models/ProdutoModel.php
@@ -0,0 +1,72 @@
+<?php
+
+class ProdutoModel
+{
+    private $caminhoArquivo;
+
+    public function __construct()
+    {
+        $this->caminhoArquivo = __DIR__ . '/../../data/produtos.json';
+
+        if (!file_exists($this->caminhoArquivo)) {
+            file_put_contents($this->caminhoArquivo, json_encode([]));
+        }
+    }
+
+    private function lerDados()
+    {
+        $json = file_get_contents($this->caminhoArquivo);
+        $dados = json_decode($json, true);
+
+        return is_array($dados) ? $dados : [];
+    }
+
+    private function salvarDados($produtos)
+    {
+        file_put_contents(
+            $this->caminhoArquivo,
+            json_encode($produtos, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)
+        );
+    }
+
+    public function buscarPorId($id)
+    {
+        $produtos = $this->lerDados();
+
+        foreach ($produtos as $produto) {
+            if ($produto['id'] == $id) {
+                return $produto;
+            }
+        }
+
+        return null;
+        
+    }
+    public function listar() 
+{ 
+return $this->lerDados(); 
+}
+
+    public function criar($dados)
+    {
+        $produtos = $this->lerDados();
+
+        $novoId = 1;
+        if (!empty($produtos)) {
+            $ids = array_column($produtos, 'id');
+            $novoId = max($ids) + 1;
+        }
+
+        $novoProduto = [
+            'id' => $novoId,
+            'nome' => $dados['nome'],
+            'codigo' => $dados['codigo'],
+            'quantidade' => (int) $dados['quantidade'],
+            'preco' => (float) $dados['preco']
+        ];
+
+        $produtos[] = $novoProduto;
+        $this->salvarDados($produtos);
+    }
+
+}
