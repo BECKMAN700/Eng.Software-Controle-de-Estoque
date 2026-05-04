1e7bcd82855fa704975cdf731bd15acad968f3b3
montreal_gsantos <N@402716>
2026-05-04 17:24:33 -0300

    adequação dos alertas para erros e sucessos ao efetuar a autenticação

--- app/Views/partials/flash.php
+++ app/Views/partials/flash.php
@@ -1,23 +1,29 @@
+<?php
+// Mensagens flash via sessão (prioridade)
+$flashSucesso = Sessao::getFlashSucesso();
+$flashErro    = Sessao::getFlashErro();
+?>
+
+<?php if ($flashSucesso !== ''): ?>
+    <div class="alert alert-success" role="alert">
+        <?= htmlspecialchars($flashSucesso) ?>
+    </div>
+<?php endif; ?>
+
+<?php if ($flashErro !== ''): ?>
+    <div class="alert alert-danger" role="alert">
+        <?= htmlspecialchars($flashErro) ?>
+    </div>
+<?php endif; ?>
+
 <?php if (!empty($mensagemSucesso)): ?>
-    <div class="alert alert-success">
+    <div class="alert alert-success" role="alert">
         <?= htmlspecialchars($mensagemSucesso) ?>
     </div>
 <?php endif; ?>
 
 <?php if (!empty($mensagemErro)): ?>
-    <div class="alert alert-danger">
+    <div class="alert alert-danger" role="alert">
         <?= htmlspecialchars($mensagemErro) ?>
-    </div>
-<?php endif; ?>
-
-<?php if (!empty($_GET['msg'])): ?>
-    <div class="alert alert-success">
-        <?= htmlspecialchars($_GET['msg']) ?>
-    </div>
-<?php endif; ?>
-
-<?php if (!empty($_GET['erro'])): ?>
-    <div class="alert alert-danger">
-        <?= htmlspecialchars($_GET['erro']) ?>
     </div>
 <?php endif; ?>
